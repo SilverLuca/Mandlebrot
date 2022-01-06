68ec9aea3753310d4a3a9d76f6cd69408eac384f
SilverLuca <91182564+SilverLuca@users.noreply.github.com>
2022-01-06 16:23:44 +0100

    updated the file

--- julia.py
+++ julia.py
@@ -1,46 +1,51 @@
 from PIL import Image
 
-size = (1024,1024)
+# Constants
+size = (2000,2000)
 black = (0,0,0)
 red = (255, 0,0)
+white = (255,255,255)
 bound = 10
+iterations = 1000
 
+# The point c of our quadratic julia funcion f_c = z^2 + c 
+c = complex(-0.08,0.696)
 
-iterations = 100
-
+# Creating the canvas
 img = Image.new( 'RGB', size, "black") 
-pixels = img.load() 
+pixels = img.load()
+print("Created canvas")
 
+# Mapping from pixel to xy-plane
 def pixel_to_xy(i,j):
     x = -2 + (4/size[0])*i
     y = 2 - (4/size[1])*j
     return( (x,y) )
-    
-def mandelbrotfunc(x,y):
+
+# Function to check whether a point stays bounded or not
+def Julia(x,y):
     z = complex(x,y)
     for _ in range(iterations):
         try:
-            z = z - (pow(z,3) -1)/(2*pow(z,2))
+            z = pow(z,2) + c # The quadratic map
         except:
             pass
         if abs(z) > bound:
             return z
     return z
     
-#print(mandelbrotfunc(0,0.5))
-
-for i in range(size[0]):    # for every col:
-    for j in range(size[1]):    # For every row
+# Loop through all pixels to colour them in correctly
+for i in range(size[0]):   
+    for j in range(size[1]):   
         coords = pixel_to_xy(i,j)
-        result = mandelbrotfunc(coords[0], coords[1])
+        result = Julia(coords[0], coords[1])
         norm = abs(result)
-        if  norm < 10:
+        if  norm < bound:
             pixels[i,j]  = black
         else:
-            pixels[i,j] = red
-    #print(f'The image is {100/size[0]*i}% done')
+            pixels[i,j] = white
 
+# Showing and saving the picture
 img.show()
-
-img.save('images/julia.png')
+img.save('images/julia3.png')
 print("done!")
